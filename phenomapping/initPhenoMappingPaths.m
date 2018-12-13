4b143ea79f6ab16532c34ac29128894b02a0262c
Anush <anush.chiappinopepe@epfl.ch>
2018-12-13 11:34:27 +0100

    ENH: accounting for bug in uigetdir latest matlabs...

--- phenomapping/initPhenoMappingPaths.m
+++ phenomapping/initPhenoMappingPaths.m
@@ -12,9 +12,9 @@ function [phenomapping_directory, thermo_data_directory] = ...
 %                           and final results (default = tmpresults folder  
 %                           in parent phenomapping folder)
 %    mattfa_directory:      Directory to the matTFA repository (default = 
-%                           empty / provide manually)
+%                           empty / provide manually* see note)
 %    texfba_directory:      Directory to the texfba repository (default = 
-%                           empty / provide manually)
+%                           empty / provide manually* see note)
 %
 % OUTPUTS:
 %    texfba_directory:      Directory to the parent phenomapping folder
@@ -29,25 +29,33 @@ if (nargin < 1)
     saving_directory = strrep(mfilename('fullpath'),...
     'phenomapping/initPhenoMappingPaths','tmpresults/');
 end
-if (nargin < 2)
-    mattfa_directory = [];
-end
-if (nargin < 3)
-    texfba_directory = [];
-end
-
 phenomapping_directory = strrep(mfilename('fullpath'),...
     'phenomapping/initPhenoMappingPaths','');
 addpath(genpath(phenomapping_directory));
 
-if isempty(mattfa_directory)
+% NOTE: it seems like uigetdir has a bug in the latest versions of 
+% matlab... this is a temporal solution - make sure you indeed install 
+% matTFA and texfba in the same paths as phenomapping. When this bug is
+% resolved the directories could be empty by default
+
+if (nargin < 2)
+    mattfa_directory = strrep(phenomapping_directory,'phenomapping/',...
+        'matTFA/');
+end
+if (nargin < 3)
+    texfba_directory = strrep(phenomapping_directory,'phenomapping/',...
+        'texfba/');
+end
+
+
+if ~isdir(mattfa_directory) || isempty(mattfa_directory)
     mattfa_directory = uigetdir('matTFA','select the matTFA directory');
 end
 addpath(genpath(mattfa_directory));
 thermo_data_directory = strcat(mattfa_directory,...
     '/thermoDatabases/thermo_data.mat');
 
-if isempty(texfba_directory)
+if ~isdir(texfba_directory) || isempty(texfba_directory)
     texfba_directory = uigetdir('texfba','select the texfba directory');
 end
 addpath(genpath(texfba_directory));
